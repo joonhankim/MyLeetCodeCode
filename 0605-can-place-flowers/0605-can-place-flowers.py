4b1dbc48487e3903740a07a4911a3628e50fd900
Joonhan Kim <37760423+joonhankim@users.noreply.github.com>
2024-04-10 21:12:31 +0900

    Time: 127 ms (80.59%), Space: 19.4 MB (12.45%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        def can_place_flower(flowerbed, n, position):
+            
+            # avoid "list index out of range"......
+            if n == 0:
+                return True
+            if position >= len(flowerbed):
+                return False
+            
+            if flowerbed[position] == 0:
+                if (position == 0 or flowerbed[position - 1] == 0) and (position == len(flowerbed) - 1 or flowerbed[position + 1] == 0):
+                    flowerbed[position] = 1
+
+                    return can_place_flower(flowerbed, n - 1, position + 2)
+                else:
+                    return can_place_flower(flowerbed, n, position + 1)
+            else:
+                return can_place_flower(flowerbed, n, position + 1)
+
+        # strat point
+        return can_place_flower(flowerbed, n, 0)
+                    
+                    
+                
+        
+        
